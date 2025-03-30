95aec0da85f854a2199bca836ccc84067382ec83
MiguelLTadeo <migueltadeo.aluno@unipampa.edu.br>
2025-03-30 18:19:22 -0300

    Atualização de códigos

---
+++ codigo.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+ 
+
+
+int main() {
+int l=1000;
+int c=1000;
+int **matriz = (int **)malloc(l * sizeof(int *));
+
+
+if (matriz == NULL) {
+printf("Erro alocacao\n");
+return 1;
+}
+
+for (int i = 0; i < l; i++) {
+matriz[i] = (int *)malloc(c * sizeof(int));
+if (matriz[i] == NULL) {
+printf("Erro alocação na linha\n");
+
+for (int j = 0; j < i; j++) {
+free(matriz[j]);
+}
+free(matriz);
+return 1;
+}
+}
+
+
+for (int i = 0; i < l; i++) {
+for (int j = 0; j < c; j++) {
+matriz[i][j] = 1;
+}
+}
+
+
+
+
+return 0;
+}
---
+++ codigo.py
@@ -0,0 +1,7 @@
+def main():
+
+    l = 1000
+
+    c = 1000
+
+    matriz = [[1 for _ in range(c)] for _ in range(l)]
