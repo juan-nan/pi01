574fc5c9f97566fe04edb28e026a5fd38673b097
Juan <juanstefani.aluno@unipampa.edu.br>
2025-04-05 17:44:06 -0300

    Update codigo.c
    correção do código

--- codigo.c
+++ codigo.c
@@ -12,30 +12,34 @@ int **matriz = (int **)malloc(l * sizeof(int *));
 
 
 if (matriz == NULL) {
-printf("Erro alocacao\n");
-return 1;
+  printf("Erro alocacao\n");
+  return 1;
 }
 
 for (int i = 0; i < l; i++) {
-matriz[i] = (int *)malloc(c * sizeof(int));
-if (matriz[i] == NULL) {
-printf("Erro alocação na linha\n");
+  matriz[i] = (int *)malloc(c * sizeof(int));
+  if (matriz[i] == NULL) {
+     printf("Erro alocação na linha\n");
 
-for (int j = 0; j < i; j++) {
-free(matriz[j]);
-}
+     for (int j = 0; j < i; j++) {
+       free(matriz[j]);
+     }
 free(matriz);
-return 1;
-}
+   return 1;
+  }
 }
 
 
 for (int i = 0; i < l; i++) {
-for (int j = 0; j < c; j++) {
-matriz[i][j] = 1;
+    for (int j = 0; j < c; j++) {
+      matriz[i][j] = 1;
+   }
 }
-}
-
+ 
+for (int j = 0; j < i; j++) {
+       free(matriz[j]);
+     }
+free(matriz);
 
 
 
